e270c3dd7ef8207142f8825099fe4bcc6f2753aa
Ali <aliaghayev132@gmail.com>
2023-01-10 10:46:45 -0800

    V1.3.1 | Custom Scroll Added |

---
+++ src/javascript/scroll.js
@@ -0,0 +1,27 @@
+{
+    const scroll = document.querySelector('.scroll');
+    const window_height = this.innerHeight;
+    const content_height = document.body.getBoundingClientRect().height;
+    const offset_y = 85;
+    let size = content_height / window_height;
+    size = window_height / size;
+    scroll.style.height = size - offset_y + 'px';
+    const rect = scroll.getBoundingClientRect();
+
+    let timer = null;
+    document.addEventListener('scroll', e => {
+        timer && clearTimeout(timer);
+        scroll.style.transition = "0"
+        scroll.style.opacity = 1;
+        let temp = window_height / (window_height - offset_y - rect.height);
+        scroll.style.top = window.scrollY / temp + offset_y + 'px';
+        timer = setTimeout(() => {
+            fadeScroll();
+        }, 3000)
+    })
+
+    function fadeScroll() {
+        scroll.style.transition = "1s"
+        scroll.style.opacity = 0;
+    }
+}
