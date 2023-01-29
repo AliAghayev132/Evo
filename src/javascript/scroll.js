a8c74b80b4d48604e60ffbd9b7ce93c167abebe0
Ali Aghayev <aliaghayev@Alis-MacBook-Air.local>
2023-01-29 12:47:35 +0400

    V1.61 | HTML & CSS & JS Fixes  |

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -1,26 +1,19 @@
 {
-    const scroll = document.querySelector('.scroll');
-    const window_height = this.innerHeight;
-    const content_height = document.body.getBoundingClientRect().height;
-    const offset_y = 85;
-    let size = content_height / window_height;
-    size = window_height / size;
-    scroll.style.height = size - offset_y + 'px';
-    const rect = scroll.getBoundingClientRect();
-    let timer = null;
-    document.addEventListener('scroll', e => {
+    let offset_y = 85;
+    const scroll = document.querySelector('.scroll'),
+        window_height = this.innerHeight,
+        content_height = document.body.getBoundingClientRect().height;
+    let scale = (content_height-offset_y)  / (window_height-offset_y),
+        timer = null;
+    scroll.style.height = window_height / scale + 'px';
+    document.addEventListener('scroll', () => {
         timer && clearTimeout(timer);
         scroll.style.transition = "0"
         scroll.style.opacity = 1;
-        let temp = window_height / (window_height - offset_y - rect.height);
-        scroll.style.top = window.scrollY / temp + offset_y + 'px';
+        scroll.style.top = ((window.scrollY-offset_y) / scale) + offset_y + 'px';
         timer = setTimeout(() => {
-            fadeScroll();
+            scroll.style.transition = "1s"
+            scroll.style.opacity = 0;
         }, 3000)
     })
-
-    function fadeScroll() {
-        scroll.style.transition = "1s"
-        scroll.style.opacity = 0;
-    }
 }
