93ca606a95fad99e8bf36e2264923e89c07e66f0
Ali Aghayev <aliaghayev@Alis-MacBook-Air.local>
2023-02-21 09:50:13 +0400

    V1.82 | Footer Changed |

--- src/javascript/pages/index.js
+++ src/javascript/pages/index.js
@@ -10,3 +10,18 @@
     })
 }
 // #endregion
+// #region
+{
+    let footerPosY = null;
+    let height = window.innerHeight;
+    const nav = document.querySelector('nav');
+    document.addEventListener('scroll', () => {
+        footerPosY = document.querySelector('footer').getBoundingClientRect().y + window.scrollY;
+        if (footerPosY < window.scrollY + height - 100) {
+            nav.classList.add('nav--close')
+        }else{
+            nav.classList.remove('nav--close')
+        }
+    })
+}
+// #endregion
